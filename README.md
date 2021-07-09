e680e06e9c87a6ca4ae1807894832dae40f529d4
ndthanhdev <ndthanhdev@outlook.com>
2021-07-10 01:17:20 +0700

    new structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# OpenMarketplace.Umbrella
---
+++ apps/shop/lib/shop.ex
@@ -0,0 +1,18 @@
+defmodule Shop do
+  @moduledoc """
+  Documentation for `Shop`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Shop.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
